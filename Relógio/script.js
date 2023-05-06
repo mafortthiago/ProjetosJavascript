42695bf564376b32ed91b2c2c90e5768e32e5eb6
Thiago Mafort <thmafort@gmail.com>
2023-05-06 19:29:35 -0300

    adiciona modo de dia ao projeto

--- Relógio/script.js
+++ Relógio/script.js
@@ -28,5 +28,38 @@ class Relogio {
     },1000);
 }
 }
+
+class ModoClaro{
+    constructor(img,main,botao){
+        this.img = document.querySelector(img);
+        this.main = document.querySelector(main);
+        this.botao = document.querySelector(botao);;
+    }
+    mudaCor(){
+        this.botao.addEventListener("click",() => {
+        if(getComputedStyle(this.main).backgroundColor == "rgb(9, 0, 17)"){
+            
+                this.main.style.backgroundColor = `rgb(241, 241, 241)`;
+                this.main.style.color = `rgb(9, 0, 17)`;
+                this.img.style.backgroundImage = `url("./src/img/fundo-claro.png")`;
+                this.img.style.boxShadow = `0px 0px 66px 21px rgb(241, 241, 241)`;
+                this.botao.style.backgroundColor = `rgb(241, 241, 241)`;
+                this.botao.textContent = "Modo noite";
+            }else{
+            
+                this.main.style.backgroundColor = `rgb(9, 0, 17)`;
+                this.main.style.color = `rgb(93, 75, 93)`;
+                this.img.style.backgroundImage = `url("./src/img/fundo-relogio.png")`;
+                this.img.style.boxShadow = `0px 0px 66px 21px rgba(51,22,51,1)`;
+                this.botao.style.backgroundColor = `rgb(9, 0, 17)`;
+                this.botao.textContent = "Modo dia";
+            
+        }
+    });
+    }
+    }
+
 const r = new Relogio("#minutos","#horas","#segundos");
 r.rotate();
+const c = new ModoClaro(".relogio-section","main",".botao");
+c.mudaCor();
